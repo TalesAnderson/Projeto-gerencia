4e3eb713cb77419c7d8848a6a8efffbe2f759896
Tales Anderson <talesandersonfla87@gmail.com>
2019-12-16 17:27:30 -0300

    Adicionando os structs e a função adiciona

---
+++ projeto.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+struct nodo{
+	char nome[50];
+	int numero;
+	char marca[50];
+	struct nodo *proximo;
+};
+typedef struct nodo Nodo;
+
+
+struct fila{
+	Nodo *inicio;
+};
+typedef struct fila Fila;
+
+
+Fila *criafila(){
+	Fila *fila=(Fila*) malloc(sizeof(Fila));
+	fila->inicio=NULL;
+	return fila;
+}
+
+void adiciona_fila(Fila *fila){
+
+
+	char nom[50];
+	int n;
+	char marc[50];
+	int x=1;
+
+	while(x!=0){
+
+   	if(fila->inicio==NULL){
+
+   	Nodo *aux=(Nodo*) malloc(sizeof(Nodo));
+
+   	printf("Informe o nome do aviao: ");
+	scanf("%s", &nom);
+	fflush(stdin);
+	printf("Informe o numero do aviao: ");
+	scanf("%d", &n);
+	fflush(stdin);
+	printf("Informe a marca do aviao: ");
+	scanf("%s", &marc);
+
+	strcpy(aux->nome, nom);
+	aux->numero=n;
+	strcpy(aux->marca, marc);
+	fila->inicio=aux;
+	aux->proximo=NULL;
+
+ }
+
+ else{
+
+ 	Nodo *novo=(Nodo*) malloc(sizeof(Nodo));
+ 	Nodo *aux2=fila->inicio;
+
+ 	printf("Informe o nome do aviao: ");
+	scanf("%s", &nom);
+	fflush(stdin);
+	printf("Informe o numero do aviao: ");
+	scanf("%d", &n);
+	fflush(stdin);
+	printf("Informe a marca do aviao: ");
+	scanf("%s", &marc);
+
+ 	while(aux2->proximo!=NULL){
+ 		aux2=aux2->proximo;
+	 }
+
+
+	strcpy(novo->nome, nom);
+	novo->numero=n;
+	strcpy(novo->marca, marc);
+	novo->proximo=NULL;
+	aux2->proximo=novo;
+
+ }
+
+
+
+	printf("Se deseja informar outro aviao digite 1, se nao digite 0: ");
+	scanf("%d", &x);
+  }
+
+}
