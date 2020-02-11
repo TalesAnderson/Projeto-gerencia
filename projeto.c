5ed5e9476f33943ab9a26387465ed166c0006b2a
Sidney002 <caiosidney69@gmail.com>
2020-02-11 08:28:31 -0300

    adicionada fila de prioridade e suas funçoes

--- projeto.c
+++ projeto.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+const int true=1;
+const int false=0;
 
 struct nodo{
 	char nome[50];
 	int numero;
 	char marca[50];
+	int prioridade;
 	struct nodo *proximo;
 };
 typedef struct nodo Nodo;
@@ -24,6 +26,13 @@ Fila *criafila(){
 	return fila;
 }
 
+int adiciona_prioridade(){
+    int prio;
+    printf("O voo possui prioridade?\n");
+    printf("1 = sim\n0 = nao\n");
+    scanf("%d",&prio);
+    return prio;
+}
 void adiciona_fila(Fila *fila){
 
 
@@ -31,8 +40,7 @@ void adiciona_fila(Fila *fila){
 	int n;
 	char marc[50];
 	int x=1;
-
-	while(x!=0){
+    int prio;
 
    	if(fila->inicio==NULL){
 
@@ -78,15 +86,10 @@ void adiciona_fila(Fila *fila){
 	novo->numero=n;
 	strcpy(novo->marca, marc);
 	novo->proximo=NULL;
+	novo->prioridade=prio;
 	aux2->proximo=novo;
 
- }
-
-
-
-	printf("Se deseja informar outro aviao digite 1, se nao digite 0: ");
-	scanf("%d", &x);
-  }
+    }
 
 }
 void imprime (Fila *fila){
@@ -95,29 +98,29 @@ void imprime (Fila *fila){
 	}
 	else{
 		Nodo *aux=fila->inicio;
-		
+
 		while (aux!=NULL){
 			printf("\nNome: %s",aux->nome);
 			printf("\nNome: %d",aux->numero);
 			printf("\nMarca: %s",aux->marca);
-			
+
 			printf("\n\n");
-			
+
 			aux=aux->proximo;
 		}
 	}
 }
 void numero_espera(Fila *fila){
-	
+
 	int contador=0;
 	Nodo *cont=fila->inicio;
-	
+
 	if(fila->inicio==NULL){
 		printf("A fila de avioes esta vazia!\n\n");
 	}
-	
+
 	else{
-		
+
 		while(cont!=NULL){
 			contador++;
 			cont=cont->proximo;
@@ -129,29 +132,44 @@ void numero_espera(Fila *fila){
 main(){
 	Fila *fila=criafila();
 	Fila *fila2=criafila();
-	
-	int x;
-	
+	Fila *fila3=criafila();
+
+	int x,p;
+
 		do{
 			printf("--------------------------------------Menu--------------------------------------\n\n");
 			printf("----------Decolagens----------\n\n");
 			printf("   ---Selecione uma opcao---\n\n");
 			printf("1 - Adciona aviao a lista de espera.\n");
 			printf("2 - Mostrar todos os avioes da fila de espera.\n");
-			printf("3 - Informar o numero de avioes na lista de espera.\n\n");
+			printf("3 - Informar o numero de avioes na lista de espera.\n");
+			printf("4 - Informar o numero de avioes na lista de espera de prioridade.\n\n");
 			printf("-----------Chegadas-----------\n\n");
 			printf("   ---Selecione uma opcao---\n\n");
-			printf("4 - Adciona aviao a lista de espera.\n");
-			printf("5 - Mostrar todos os avioes da fila de espera.\n");
-			printf("6 - Informar o numero de avioes na lista de espera.\n\n");
-			printf("0 - para sair do programa.\n\n");
+			printf("5 - Adciona aviao a lista de espera.\n");
+			printf("6 - Mostrar todos os avioes da fila de espera.\n");
+			printf("7 - Informar o numero de avioes na lista de espera.\n");
+			printf("8 - Informar o numero de avioes na lista de espera de prioridade.\n\n");
+			printf("9 - para sair do programa.\n\n");
 			scanf("%d",&x);
 			switch(x){
-				
+
 				case 1:
-					adiciona_fila(fila);
-					system("pause   ");
-					printf("\n\n");
+				    while(x!=0){
+				    p=adiciona_prioridade();
+				    if(p==true){
+                        adiciona_fila(fila3);
+                        system("pause   ");
+                        printf("\n\n");
+				    }
+				    else{
+                        adiciona_fila(fila);
+                        system("pause   ");
+                        printf("\n\n");
+				    }
+				    printf("Se deseja informar outro aviao digite 1, se nao digite 0: ");
+                    scanf("%d", &x);
+				    }
 					break;
 				case 2:
 					imprime(fila);
@@ -163,23 +181,45 @@ main(){
 					system("pause   ");
 					printf("\n\n");
 					break;
-				case 4:
+                case 4:
+                numero_espera(fila3);
+                system("pause   ");
+                printf("\n\n");
+                break;
+				case 5:
+				    while(x!=0){
+                    p=adiciona_prioridade();
+				    if(p==true){
+                    adiciona_fila(fila3);
+					system("pause   ");
+					printf("\n\n");
+                    }
+                    else{
 					adiciona_fila(fila2);
 					system("pause   ");
 					printf("\n\n");
+                    }
+                    printf("Se deseja informar outro aviao digite 1, se nao digite 0: ");
+                    scanf("%d", &x);
+				    }
 					break;
-				case 5:
+				case 6:
 					imprime(fila2);
 					system("pause   ");
 					printf("\n\n");
 					break;
-				case 6:
+				case 7:
 					numero_espera(fila2);
 					system("pause   ");
 					printf("\n\n");
 					break;
+                case 8:
+					numero_espera(fila3);
+					system("pause   ");
+					printf("\n\n");
+					break;
 			}
-			
-		}while(x!=0);
+
+		}while(x!=9);
 }
 
